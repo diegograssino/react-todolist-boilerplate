bcc6d58ab26568360af861967e3c892f9b542192
Diego Grassino <diegograssino@gmail.com>
2022-09-23 20:04:02 -0300

    Needs to add a filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,73 +2,128 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [inputTask, setInputTask] = useState('');
+  const [inputValue, setInputValue] =
+    useState('');
   const [idCount, setIdCount] = useState(0);
+
+  useEffect(() => {
+    const tAux = tasks.filter(
+      t => t.list !== 'inprocess'
+    );
+
+    console.log(tAux);
+  }, [tasks]);
 
   const addTask = t => {
     if (t) {
-      setTasks([...tasks, { id: idCount, name: t, list: 'in-process' }]);
+      setTasks([
+        ...tasks,
+        {
+          id: idCount,
+          name: t,
+          list: 'inprocess',
+        },
+      ]);
+      setInputValue('');
       setIdCount(idCount + 1);
-      setInputTask('');
     }
   };
 
-  useEffect(() => {
+  const delTask = id => {
+    setTasks(
+      tasks.filter(task => task.id !== id)
+    );
+  };
+
+  const moveTask = t => {
+    const pos = tasks.indexOf(t);
+    const tasksAux = tasks;
+    tasksAux[pos].list = 'finished';
+    setTasks(tasksAux);
     console.log(tasks);
-    console.log('idCount: ' + idCount);
-  }, [tasks, idCount]);
+  };
 
   return (
     <div className="flex flex-column flex-centered full-height">
       <div className="card margin-bottom">
-        <h1 className="text-centered margin-bottom">📘 To-Do List</h1>
+        <h1 className="text-centered margin-bottom">
+          📘 To-Do List
+        </h1>
         <div className=" flex flex-space-between">
           <input
             type="text"
             placeholder="Type here a task ..."
-            onChange={e => setInputTask(e.target.value)}
-            value={inputTask}
+            onChange={e =>
+              setInputValue(e.target.value)
+            }
+            value={inputValue}
           />
-          <button className="padding-y" onClick={e => addTask(inputTask)}>
+          <button
+            className="padding-x"
+            onClick={() => addTask(inputValue)}
+          >
             ⏳
           </button>
         </div>
       </div>
       <div className="card margin-bottom">
-        <h2 className="margin-bottom">In Process</h2>
+        <h2 className="margin-bottom">
+          In Process
+        </h2>
         <ul>
-          <li className="flex flex-space-between margin-bottom">
-            ⏳ Task
-            <div>
-              <button>🎉</button> <button>❌</button>
-            </div>
-          </li>
-          <li className="flex flex-space-between margin-bottom">
-            ⏳ Another task
-            <div>
-              <button>🎉</button> <button>❌</button>
-            </div>
-          </li>
+          {tasks.map((task, i) => (
+            <li
+              className="flex flex-space-between margin-bottom"
+              key={i}
+            >
+              ⏳ {task.name}
+              <div>
+                <button
+                  onClick={() => moveTask(task)}
+                >
+                  🎉
+                </button>{' '}
+                <button
+                  onClick={() => delTask(task.id)}
+                >
+                  ❌
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
-        <p className="margin-bottom">Nothing here! 🤷‍♀️</p>
+        {tasks.length === 0 && (
+          <p className="margin-bottom">
+            Nothing here! 🤷‍♀️
+          </p>
+        )}
       </div>
       <div className="card margin-bottom">
-        <h2 className="margin-bottom">Finished</h2>
+        <h2 className="margin-bottom">
+          Finished
+        </h2>
         <ul>
-          <li className="flex flex-space-between margin-bottom">
-            🎉 Task
-            <div>
-              <button>❌</button>
-            </div>
-          </li>
-          <li className="flex flex-space-between margin-bottom">
-            🎉 Another task
-            <div>
-              <button>❌</button>
-            </div>
-          </li>
+          {tasks.map((task, i) => (
+            <li
+              className="flex flex-space-between margin-bottom"
+              key={i}
+            >
+              🎉 {task.name}
+              <div>
+                <button
+                  onClick={() => delTask(task.id)}
+                >
+                  ❌
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
-        <p className="margin-bottom">Nothing here! 🤷‍♀️</p>
+        {tasks.length === 0 && (
+          <p className="margin-bottom">
+            Nothing here! 🤷‍♀️
+          </p>
+        )}
       </div>
     </div>
   );
