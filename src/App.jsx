b1bde0b5ca1f85eb9452ee7ab44b69ea8f673b52
diego.grassino <diegograssino@gmail.com>
2022-09-23 18:07:05 -0300

    Add initial functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [inputTask, setInputTask] = useState('');
+  const [idCount, setIdCount] = useState(0);
+
+  const addTask = t => {
+    if (t) {
+      setTasks([...tasks, { id: idCount, name: t, list: 'in-process' }]);
+      setIdCount(idCount + 1);
+      setInputTask('');
+    }
+  };
+
+  useEffect(() => {
+    console.log(tasks);
+    console.log('idCount: ' + idCount);
+  }, [tasks, idCount]);
+
   return (
     <div className="flex flex-column flex-centered full-height">
       <div className="card margin-bottom">
         <h1 className="text-centered margin-bottom">📘 To-Do List</h1>
         <div className=" flex flex-space-between">
-          <input type="text" placeholder="Type here a task ..." />
-          <button className="padding-y">⏳</button>
+          <input
+            type="text"
+            placeholder="Type here a task ..."
+            onChange={e => setInputTask(e.target.value)}
+            value={inputTask}
+          />
+          <button className="padding-y" onClick={e => addTask(inputTask)}>
+            ⏳
+          </button>
         </div>
       </div>
       <div className="card margin-bottom">
